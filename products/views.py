d08d53b13143925b3ec9867209dabd2b772e6295
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2024-09-05 21:36:30 -0500

    add: djangorestframework

--- products/views.py
+++ products/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import FormView, ListView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from products.forms import ProductForm
 from products.models import Product
+from .serializers import ProductSerializer
 
 
 # Create your views here.
@@ -21,3 +24,12 @@ class ProductListView(ListView):
     model = Product
     template_name = "products/list.html"
     context_object_name = "products"
+
+class ProductListAPI(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, _):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
